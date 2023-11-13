70d0fba7c8c4ecbb30fb62783b4bb32e683943d9
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-13 09:37:57 -0500

    fixing syntax errors

--- src/CrowdFundManager.sol
+++ src/CrowdFundManager.sol
@@ -1,24 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-/*
-Crowdfunding ERC20 contract
-Your contract should have a createFundraiser() function with a goal and a deadline as arguments. Donators can donate() to a given fundraiserId.
-If the goal is reached before the deadline, the wallet that called createFundraiser() Can withdraw() all the funds associated with that campaign.
-Otherwise, if the deadline passes without reaching the goal, the donators can withdraw their donation.
-Build a contract that supports Ether and another that supports ERC20 tokens.
-
-Some corner cases to think about:
-what if the same address donates multiple times?
-what if the same address donates to multiple different campaigns?
-*/
-
-
 contract CrowdFundManager {
 
-    event FundraiserCreated(uint256 fundraiserId, string name, uint256 goalAmount, uint256 deadline, address starter, address token);
-    event DonationReceived(uint256 fundraiserId, address donator, uint256 amount);
-    event DonationWithdrawn(uint256 fundraiserId, address donator, uint256 amount);
+    event FundraiserCreated(uint256 indexed fundraiserId, string name, uint256 goalAmount, uint256 deadline, address starter, address token);
+    event DonationReceived(uint256 indexed fundraiserId, address donator, uint256 amount);
+    event DonationWithdrawn(uint256 indexed fundraiserId, address donator, uint256 amount);
 
     struct Fundraiser {
         uint256 fundraiserId;
@@ -28,72 +15,60 @@ contract CrowdFundManager {
         uint256 deadline;
         address starter;
         address token;
-        Donator[] donators;
+        mapping (address => uint256) donations;
     }
-
-    struct Donator {
-        uint256 fundraiserId;
-        address donator;
-        uint256 amount;
-    }
-
-    uint256 public totalEthDeposits;
-    mapping (address => Donator) public ethDonations;
 
     mapping(uint256 => Fundraiser) public fundraisers;
-    uint256 public fundraiserId = 0;
+    uint256 public fundraiserId;
 
-    function createFundraiser(string memory name,
-        uint256 goalAmount,
-        uint256 deadline,
-        address token) {
-        require(deadline > block.timestamp, "deadline must be in the future");
-        Donator memory donator = Donator(fundraiserId, msg.sender, 0);
-        Fundraiser newFundraiser = Fundraiser(fundraiserId, name, goalAmount, 0, deadline, msg.sender, token, [donator]);
-        fundraisers[fundraiserId] = newFundraiser;
+    function createFundraiser(string memory _name, uint256 _goalAmount, uint256 _deadline, address _token) public {
+        require(_deadline > block.timestamp, "Deadline must be in the future");
+        Fundraiser storage newFundraiser = fundraisers[fundraiserId];
+        newFundraiser.fundraiserId = fundraiserId;
+        newFundraiser.name = _name;
+        newFundraiser.goalAmount = _goalAmount;
+        newFundraiser.deadline = _deadline;
+        newFundraiser.starter = msg.sender;
+        newFundraiser.token = _token;
+
+        emit FundraiserCreated(fundraiserId, _name, _goalAmount, _deadline, msg.sender, _token);
         fundraiserId++;
-        emit FundraiserCreated(fundraiserId, name, goalAmount, deadline, msg.sender, token);
     }
 
-    function donate(uint256 _fundraiserId) payable {
-        require(msg.value > 0, "donation must be greater than 0");
-        require(fundraisers[_fundraiserId].deadline > block.timestamp, "deadline has passed");
+    function donate(uint256 _fundraiserId) public payable {
+        require(msg.value > 0, "Donation must be greater than 0");
+        require(fundraisers[_fundraiserId].deadline > block.timestamp, "Deadline has passed");
+
         Fundraiser storage fundraiser = fundraisers[_fundraiserId];
-        totalEthDeposits += msg.value;
-        ethDonations[msg.sender] = Donator(_fundraiserId, msg.sender, msg.value);
+        fundraiser.currentAmount += msg.value;
+        fundraiser.donations[msg.sender] += msg.value;
+
         emit DonationReceived(_fundraiserId, msg.sender, msg.value);
     }
 
-    function withdraw(uint256 _fundraiserId) {
-        if (fundraisers[_fundraiserId].deadline > block.timestamp && fundraisers[_fundraiserId].goalAmount >= fundraisers[_fundraiserId].currentAmount) {
-            require(msg.sender == fundraisers[_fundraiserId].starter, "only start can withdraw");
-            address payable recipient = payable(fundraisers[_fundraiserId].starter);
-            (bool success, ) = recipient.call{value: fundraisers[_fundraiserId].currentAmount}("");
-            require(success, "Transfer failed.");
-            totalEthDeposits -= fundraisers[_fundraiserId].currentAmount;
-            fundraisers[_fundraiserId].currentAmount = 0;
-            emit DonationWithdrawn(_fundraiserId, msg.sender, fundraisers[_fundraiserId].currentAmount);
-        } else {
-            if (isDonator(_fundraiserId, msg.sender)) {
-                address payable recipient = payable(msg.sender);
-                (bool success, ) = recipient.call{value: ethDeposits[msg.sender].amount}("");
-                require(success, "Transfer failed.");
-                totalEthDeposits -= ethDeposits[msg.sender].amount;
-                ethDonations[msg.sender].amount = 0;
-                emit DonationWithdrawn(_fundraiserId, msg.sender, ethDeposits[msg.sender].amount);
-            }
+    function withdraw(uint256 _fundraiserId) public {
+        Fundraiser storage fundraiser = fundraisers[_fundraiserId];
 
+        if (block.timestamp <= fundraiser.deadline && fundraiser.currentAmount >= fundraiser.goalAmount) {
+            require(msg.sender == fundraiser.starter, "Only starter can withdraw");
+
+            uint256 amount = fundraiser.currentAmount;
+            fundraiser.currentAmount = 0;
+            payable(fundraiser.starter).transfer(amount);
+
+            emit DonationWithdrawn(_fundraiserId, msg.sender, amount);
+        } else if (block.timestamp > fundraiser.deadline) {
+            uint256 donorAmount = fundraiser.donations[msg.sender];
+            require(donorAmount > 0, "No donations to withdraw");
+
+            fundraiser.donations[msg.sender] = 0;
+            payable(msg.sender).transfer(donorAmount);
+
+            emit DonationWithdrawn(_fundraiserId, msg.sender, donorAmount);
         }
     }
 
-    function isDonator(uint256 _fundraiserId, address potentialDonator) public returns(bool) {
-        for (uint i = 0; i < fundraisers[_fundraiserId].donators.length; i++) {
-            if (fundraisers[_fundraiserId].donators[i] == potentialDonator) {
-                return true;
-            }
-        }
-
-        return false;
+    function isDonator(uint256 _fundraiserId, address _potentialDonator) public view returns(bool) {
+        return fundraisers[_fundraiserId].donations[_potentialDonator] > 0;
     }
-
 }
