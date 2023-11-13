100343585c7db50d56c591786678792061f50d3b
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-13 09:51:38 -0500

    Adding import

--- src/CrowdFundManagerERC20.sol
+++ src/CrowdFundManagerERC20.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
-
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract CrowdFundManagerERC20 {
+
+
+
+
 
 }
