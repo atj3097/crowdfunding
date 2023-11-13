790defd7849f5e6b753a87701c8428cf2fa934de
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-13 09:52:19 -0500

    Adding fundraiser data type

--- src/CrowdFundManagerERC20.sol
+++ src/CrowdFundManagerERC20.sol
@@ -4,7 +4,16 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract CrowdFundManagerERC20 {
 
-
+    struct Fundraiser {
+        uint256 fundraiserId;
+        string name;
+        uint256 goalAmount;
+        uint256 currentAmount;
+        uint256 deadline;
+        address starter;
+        IERC20 token;
+        mapping (address => uint256) donations;
+    }
 
 
 
